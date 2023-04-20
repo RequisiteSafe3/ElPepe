0722cec4285306257fffc9a7f30c01d8b7644f6b
Maurisio69 <mpinazavala@gmail.com>
2023-04-19 20:20:37 -0600

    YA JALA LA AÑAÑAAAA

---
+++ ElPepe/Assets/scripts/añañaTres.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class añañaTres : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+    public SpriteRenderer spriteR;
+    public static bool x = true;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (x == true)
+        {
+            rb2D.velocity = new Vector2(-1.5f, 0); //derecha
+            spriteR.flipX = false;
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(1.5f, 0); //derecha
+            spriteR.flipX = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Mover"))
+        {
+            if (x == true)
+            {
+                x = false;
+            }
+            else
+            {
+                x = true;
+            }
+        }
+        else if (collision.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("pene");
+        }
+    }
+}
