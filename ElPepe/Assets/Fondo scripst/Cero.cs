fbc11a397df647156c310a603424f19826a8cf2a
Maurisio69 <mpinazavala@gmail.com>
2023-05-04 09:27:03 -0600

    no se

--- ElPepe/Assets/Fondo scripst/Cero.cs
+++ ElPepe/Assets/Fondo scripst/Cero.cs
@@ -6,18 +6,23 @@ public class Cero : MonoBehaviour
 {
     public Rigidbody2D rb2D;
     public float runSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    [SerializeField] private float multiplierValue = 1.5f;
+    private float runMultiplier;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        { // Running.
+            runMultiplier = multiplierValue;
+        }
+        else
+        {
+            runMultiplier = 1f;
+        }
         if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A))
         {
-            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            rb2D.velocity = new Vector2(runSpeed * runMultiplier, rb2D.velocity.y);
         }
         else
         {
