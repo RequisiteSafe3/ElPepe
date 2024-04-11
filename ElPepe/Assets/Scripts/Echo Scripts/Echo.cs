478ebdf320841e9c3ddf03df6f4b21d45b97ff99
Maurisio69 <mpinazavala@gmail.com>
2024-04-10 22:52:12 -0600

    Avance al nuevo echo

---
+++ ElPepe/Assets/Scripts/Echo Scripts/Echo.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Echo : MonoBehaviour
+{
+    public bool Intangible = false;
+    public bool Mascara = false;
+    public Animator animator;
+
+    public GameObject Arbolito;
+    public GameObject ible;
+    public GameObject time;
+    public GameObject m;
+
+    private bool plantar = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I) && Intangible == false)
+        {
+            Intangible = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && Intangible == true)
+        {
+            Intangible = false;
+        }
+        //plantar
+        if (Input.GetKeyDown(KeyCode.E) && plantar == true)
+        {
+            Instantiate(Arbolito, new Vector3(transform.position.x, transform.position.y - 0.13f, -0.1f), Quaternion.identity);
+        }
+
+        //animaciones
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            
+        }
+
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A))
+        { //Dont move when is pressing both keyes
+            animator.SetBool("idle", true);
+            animator.SetBool("run", false);
+            animator.SetBool("walk", false);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+            { // Running.
+                animator.SetBool("walk", true); /* Animator */
+                animator.SetBool("run", false); /* Animator */
+            }
+            else
+            { // Idle animation in case of not moving.
+                animator.SetBool("walk", false); /* Animator */
+                animator.SetBool("run", false); /* Animator */
+                animator.SetBool("idle", true); /* Animator */
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Dead") && Intangible == false)
+        {
+            Debug.Log("sE MUERES");
+        }
+        else if (collision.CompareTag("masc"))
+        {
+            StartCoroutine("Mascara_");
+            StartCoroutine("ani_");
+        }
+        else if (collision.CompareTag("humo") && Intangible == false && Mascara == false)
+        {
+            Debug.Log("sE MUERES");
+        }
+        else if (collision.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (collision.CompareTag("arbol"))
+        {
+            plantar = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("arbol"))
+        {
+            plantar = false;
+        }
+    }
+    IEnumerator Mascara_()
+    {
+        Mascara = true;
+        ible.gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime(10);
+        Masca();
+    }
+    IEnumerator ani_()
+    {
+        time.gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime(10.75f);
+        time.gameObject.SetActive(false);
+    }
+    private void Masca()
+    {
+        ible.gameObject.SetActive(false);
+        Mascara = false;
+    }
+}
